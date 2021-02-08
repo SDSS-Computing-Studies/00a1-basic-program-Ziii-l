a952a243f2445a936617bb9237feef65ca79df69
Ziii-l <ziweil101@deltalearns.ca>
2021-02-08 13:27:26 -0800

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -4,5 +4,5 @@
 #  Use of the the print() and print( , end="") commands to display output.
 #  Check the README.md file for the sample output
 
-print("Hello world!")
-print(" This is how multiple print statements are joined. Use end=""" ,end="")
+print("Hello world!",end=" " )
+print("This is how multiple print statements are joined. Use end=\"\"" ,end="")
